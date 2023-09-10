64b083fd730de4d5a4f4a2c87e58687666291c91
Alin Bumbes <alinbumbes@gmail.com>
2023-09-10 22:36:55 +0300

    fork for another proj

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
 'use strict';
-//select svg element
-const svgElem = document.querySelector('.svg-bg-colors');
-
-//set css variable for svg element to be the largest of the two dimensions
-const setPropForSvg = () =>
-    window.innerHeight * 2 <= window.innerWidth
-        ? svgElem.style.setProperty('--width-or-height-largest', window.innerWidth + 'px')
-        : svgElem.style.setProperty('--width-or-height-largest', window.innerHeight * 2 + 'px');
-
-window.addEventListener('resize', setPropForSvg);
-setPropForSvg();
