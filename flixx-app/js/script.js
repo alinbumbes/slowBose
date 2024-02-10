c2b5ca7062dda5b7eba0053151320c0021b932f3
Alin Bumbes <alinbumbes@gmail.com>
2024-02-10 20:21:54 +0200

    tv shows working sort of

--- flixx-app/js/script.js
+++ flixx-app/js/script.js
@@ -9,11 +9,12 @@ console.log(global.currentPage)
 async function fetchAPIData(endpoint) {
   const API_KEY = 'a8500c5a90558ec8ae375b1874501089'
   const API_URL = 'https://api.themoviedb.org/3/'
-
+  showSpinner()
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
   )
   const data = await response.json()
+  hideSpinner()
   return data
 }
 
@@ -39,7 +40,35 @@ async function displayPopularMovies() {
     </div>
     `
     document.querySelector('#popular-movies').appendChild(div)
-    console.log(result)
+  })
+
+//   console.log(result)
+    
+}
+
+// display popular shows
+async function displayPopularShows() {
+  const { results } = await fetchAPIData('tv/popular')
+
+  results.forEach((show) => {
+    const div = document.createElement('div')
+    div.classList.add('card')
+    div.innerHTML = `
+    
+    <a href="movie-details.html?id=${show.id}">   
+    ${show.poster_path 
+        ? `<img src="https://image.tmdb.org/t/p/w500/${show.poster_path}" class="card-img-top" alt="Movie Title" />` 
+        : `<img src="images/no-image.jpg" class="card-img-top" alt="${show.name}" />`
+    }
+    </a>
+    <div class="card-body">
+        <h5 class="card-title">${show.original_name}</h5>
+        <p class="card-text">
+            <small class="text-muted">Release: ${show.first_air_date}</small>
+        </p>
+    </div>
+    `
+    document.querySelector('#popular-shows').appendChild(div)
   })
 
 //   console.log(result)
@@ -56,6 +85,15 @@ function highlightActiveLink() {
   })
 }
 
+function showSpinner() {
+document.querySelector('.spinner').classList.add('show')
+}
+
+function hideSpinner() {
+  document.querySelector('.spinner').classList.remove('show')
+}
+
+
 function init() {
   switch (global.currentPage) {
     case '/flixx-app/':
@@ -66,6 +104,7 @@ function init() {
 
     case '/flixx-app/shows.html':
       console.log('Shows')
+      displayPopularShows()
       break
 
     case '/flixx-app/movie-details.html':
@@ -76,7 +115,7 @@ function init() {
       console.log('TV Details')
       break
 
-    case '/search.html':
+    case '/flixx-app/search.html':
       console.log('Search')
       break
 
